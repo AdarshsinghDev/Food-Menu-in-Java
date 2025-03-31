0f562f9df2f24a2bc86f9b5a1888bb9562bba295
Adarsh Singh <mrideal000@gmail.com>
2025-03-31 21:50:15 +0530

    My first commit

--- Main.java
+++ Main.java
@@ -2,71 +2,24 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        String item;
-        double price;
-        int quantity;
-
         Scanner scanner = new Scanner(System.in);
 
-        // Menu of Food;
+        String email;
+        String username;
+        String domain;
 
-        System.out.println("Pizza : 18.3$");
-        System.out.println("Sandwich : 70$");
-        System.out.println("Burger : 90$");
-        System.out.println("Noodles : 87.32$");
-        System.out.println("Chicken : 180$");
-        System.out.println("Rajama : 170$");
+        System.out.print("Enter Email: ");
+        email = scanner.nextLine();
 
-        // User Choose Making
-        System.out.print("What you Want to Eat? :");
-        item = scanner.nextLine();
-
-        switch (item) {
-
-            case "Pizza":
-                System.out.print("Enter here Quantity: ");
-                quantity = scanner.nextInt();
-                price = 18.3 * quantity;
-                System.out.print("Your Total Amount to pay: $" + price);
-                break;
-
-            case "Sandwich":
-                System.out.print("Enter here Quantity: ");
-                quantity = scanner.nextInt();
-                price = 70 * quantity;
-                System.out.print("Your Total Amount to pay: $" + price);
-                break;
-
-            case "Burger":
-                System.out.print("Enter here Quantity: ");
-                quantity = scanner.nextInt();
-                price = 90 * quantity;
-                System.out.print("Your Total Amount to pay: $" + price);
-                break;
-
-            case "Noodles":
-                System.out.print("Enter here Quantity: ");
-                quantity = scanner.nextInt();
-                price = 87.32 * quantity;
-                System.out.print("Your Total Amount to pay: $" + price);
-                break;
-
-            case "Chicken":
-                System.out.print("Enter here Quantity: ");
-                quantity = scanner.nextInt();
-                price = 180 * quantity;
-                System.out.print("Your Total Amount to pay: $" + price);
-                break;
-
-            case "Rajama":
-                System.out.print("Enter here Quantity: ");
-                quantity = scanner.nextInt();
-                price = 170 * quantity;
-                System.out.print("Your Total Amount to pay: $" + price);
-                break;
-
-            default:
-                break;
+        if (email.isEmpty()) {
+            System.out.println("Email is Empty");
+        } else if (email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@")+1);
+            System.out.println("Your username: "+username);
+            System.out.println(domain);
+        } else {
+            System.out.println("Entered Email is wrong!");
         }
 
         scanner.close();
